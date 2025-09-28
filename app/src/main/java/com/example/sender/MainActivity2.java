5cfbb242358d46c3b7c1033fc1ea5aafb51d77b9
Vasya <br2876@bs.amalnet.k12.il>
2025-09-28 19:02:39 +0300

    merrage

--- app/src/main/java/com/example/sender/MainActivity2.java
+++ app/src/main/java/com/example/sender/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.sender;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button returno;
+    TextView note;
+    TextView x1;
+    TextView x2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        returno = findViewById(R.id.returno);
+        note = findViewById(R.id.note);
+        x1 = findViewById(R.id.x1);
+        x2 = findViewById(R.id.x2);
+
+
+
 
     }
 }
