975453a601fbcab1887c59bbba302e44a4be5b1c
Vasya <br2876@bs.amalnet.k12.il>
2025-09-28 17:03:34 +0300

    all we need in the first activity

--- app/src/main/java/com/example/sender/MainActivity.java
+++ app/src/main/java/com/example/sender/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sender;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -10,13 +11,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
+
 public class MainActivity extends AppCompatActivity {
 
-    Button random;
-    Button see;
-    EditText inpa;
-    EditText inpb;
-    EditText inpc;
+    private Button random;
+    private Button see;
+    private EditText inpa;
+    private EditText inpb;
+    private EditText inpc;
+
+    private int a;
+    private int b;
+    private int c;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +39,37 @@ public class MainActivity extends AppCompatActivity {
         inpb = findViewById(R.id.inpb);
         inpc = findViewById(R.id.inpc);
 
+    }
 
+    public void set_values() {
+        String a1 = inpa.getText().toString();
+        if (!a1.isEmpty()) a = Integer.parseInt(a1);
+        else a = 1;
+
+        String b1 = inpb.getText().toString();
+        if (!b1.isEmpty()) b = Integer.parseInt(b1);
+        else b = 1;
+
+        String c1 = inpc.getText().toString();
+        if (!c1.isEmpty()) c = Integer.parseInt(c1);
+        else c = 1;
     }
 
 
+
+    public void send(View view) {
+        set_values();
+    }
+
+    public void put(View view) {
+        Random far = new Random();
+        a = far.nextInt();
+        b = far.nextInt();
+        c = far.nextInt();
+
+        inpa.setText(String.valueOf(a));
+        inpb.setText(String.valueOf(b));
+        inpc.setText(String.valueOf(c));
+
+    }
 }
