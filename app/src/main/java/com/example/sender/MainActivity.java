9a14b4a3661c0e8293c2ecac057a68f4be8bac41
Vasya <br2876@bs.amalnet.k12.il>
2025-09-28 15:58:46 +0300

    merrage

--- app/src/main/java/com/example/sender/MainActivity.java
+++ app/src/main/java/com/example/sender/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sender;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button random;
+    Button see;
+    EditText inpa;
+    EditText inpb;
+    EditText inpc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        random = findViewById(R.id.random);
+        see = findViewById(R.id.see);
+
+        inpa = findViewById(R.id.inpa);
+        inpb = findViewById(R.id.inpb);
+        inpc = findViewById(R.id.inpc);
+
+
     }
+
+
 }
